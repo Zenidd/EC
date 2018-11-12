c871e0da1f30fc78bfe90e64898956f073f553b2
Zenidd <jpsaezgutierrez@gmail.com>
2018-11-12 10:19:12 +0100

    Delete bateriap.sh

--- P2/bateriap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 19); do
-rm media
-gcc -x assembler-with-cpp -D TEST=$i -no-pie -nostartfiles media.s -o media
-echo "-------------------------------------------" 
-echo -n "T#$i "; ./media
-echo "-------------------------------------------" 
-done
